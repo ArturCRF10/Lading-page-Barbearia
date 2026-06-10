bd45a32feaf2a0972c23fc463ab5923345670442
ArturCRF10 <arturanjos726@gmail.com>
2026-06-09 23:13:33 -0300

    FIMgit add .!

--- src/img/scripts/index.js
+++ src/img/scripts/index.js
@@ -13,3 +13,40 @@ const menu = document.querySelector('.links-incio');
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+const track = document.querySelector(".track");
+const imagens = document.querySelectorAll(".track img");
+
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+
+let indice = 0;
+
+function moverCarrossel() {
+    const largura = 300;
+
+    track.style.transform = `translateX(-${indice * largura}px)`;
+}
+
+btnNext.addEventListener("click", () => {
+    indice++;
+
+    if (indice >= imagens.length) {
+        indice = 0;
+    }
+
+    moverCarrossel();
+});
+
+btnPrev.addEventListener("click", () => {
+    indice--;
+
+    if (indice < 0) {
+        indice = imagens.length - 1;
+    }
+
+    moverCarrossel();
+});
+
+
+        
